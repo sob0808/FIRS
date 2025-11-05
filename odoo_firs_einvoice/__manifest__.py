ae064d0c23fe579bd36687842cbd73e412fc812d
sob0808 <sobsinesob@gmail.com>
2025-11-05 13:12:35 +0100

    V1

--- odoo_firs_einvoice/__manifest__.py
+++ odoo_firs_einvoice/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "FIRS e-Invoice Integration",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "summary": "Automatic connection to FIRS e-Invoicing (TaxPro Max)",
     "category": "Accounting",
     "author": "Your Company",
