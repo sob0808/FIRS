2d5be7093a22f390a148ce2be755403436fff105
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2025-11-10 10:09:18 +0100

    update firs module

--- odoo_firs_einvoice/__manifest__.py
+++ odoo_firs_einvoice/__manifest__.py
@@ -1,20 +1,19 @@
+# -*- coding: utf-8 -*-
 {
     "name": "FIRS e-Invoice Integration",
-    "version": "1.1.0",
+    "version": "1.1.2",
     "summary": "Automatic connection to FIRS e-Invoicing (TaxPro Max)",
     "category": "Accounting",
     "author": "Your Company",
-    "depends": [
-        "account"
-    ],
+    "depends": ["account"],
     "data": [
-        "security/ir.model.access.csv",
         "views/firs_config_views.xml",
         "views/account_move_inherit_views.xml",
         "report/invoice_report_templates.xml",
-        "data/ir_cron.xml"
+        "data/ir_cron.xml",
+        "data/ir_actions_server.xml",
     ],
-    "installable": true,
-    "application": false,
-    "license": "LGPL-3"
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
 }
